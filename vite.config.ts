c53266fdbfaa434589345c723d4891c22949784c
unknown <you@example.commashhhadwaterquality@gmail.com>
2025-12-24 21:40:58 +0330

    Automatic deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // استفاده از مسیر نسبی برای سازگاری با GitHub Pages
+  base: '/aquatrack/', // تنظیم دقیق مسیر پایه مطابق با نام مخزن گیت‌هاب
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
   },
